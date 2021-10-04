ca48405aea689c4073bf5c9fc1239ca861038b79
root <henokmesfin2121@gmail.com>
2021-10-04 11:05:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value of the passed parameter and assign a value 98
+ * @n: is a pointer variable that stores the address of the passed argument
+ * return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
