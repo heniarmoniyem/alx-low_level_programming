7b95072bf7b73addea749a57e416abae6ddd2f06
root <henokmesfin2121@gmail.com>
2021-10-04 11:22:51 -0700

    Rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * more_numbers - prints 10 times the numbers from 0 to 14.
- * followed by a new line.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void more_numbers(void)
+int main(void)
 {
-	int h, g;
+  char s[10] = "My School";
 
-	for (h = 0; h < 10; h++)
-	{
-		for (g = 0; g < 15; g++)
-		{
-			if (g >= 10)
-				putchar(g / 10 + '0');
-			putchar(g % 10 + '0');
-		}
-		putchar('\n');
-	}
+  printf("%s\n", s);
+  rev_string(s);
+  printf("%s\n", s);
+  return (0);
 }
